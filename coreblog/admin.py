c7ccae65a2fed7b7075fc82e18ad000cce193189
abdurahman-hassan <abdelrahman15376@fagr.bu.edu.eg>
2023-06-04 01:05:00 +0300

    Add PostAdmin and CommentAdmin

--- coreblog/admin.py
+++ coreblog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from coreblog.models import Tag, Author, Post, Comment
+
 # Register your models here.
+
+admin.site.register(Tag)
+admin.site.register(Author)
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'email_field', 'post']
+
+
+admin.site.register(Comment, CommentAdmin)
